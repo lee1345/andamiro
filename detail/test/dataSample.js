752a21b90a731b741840f09a0a3a7e1faa3aa700
kimhyemin <ktyy1198@naver.com>
2024-10-10 17:10:19 +0900

    굳굳

--- detail/test/dataSample.js
+++ detail/test/dataSample.js
@@ -2,7 +2,7 @@ const agent = {name: '김길동', image: './img/heart.jpg'
 }
 
 const gift = [
-    {name: '박길동', price: 1000, image: './img/heart.jpg'},
+    { id :1, name: '박길동', price: 1000, image: './img/heart.jpg'},
     {name: '최길동', price: 2000, image: './img/people.png'}
 ];
 
