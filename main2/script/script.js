4c40ce567ab531fd9897acc7b12e955de10fbba5
kimhyemin <ktyy1198@naver.com>
2024-10-14 12:04:29 +0900

    'css3차수정'

--- main2/script/script.js
+++ main2/script/script.js
@@ -249,3 +249,22 @@ fetch('./header.html')
 .then(data => {
     document.getElementById('navbar').innerHTML = data;
 });
+
+
+
+
+
+let container = document.getElementById('container')
+
+toggle = () => {
+  container.classList.toggle('sign-in')
+  container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+  container.classList.add('sign-in')
+}, 200)
+
+
+
+Resources
