56f1ddb20df3b56efc102488ea6ce4b60d2ef58a
KS <imshinzle@naver.com>
2024-10-15 14:11:24 +0900

    최종연결

--- final/script/script.js
+++ final/script/script.js
@@ -153,12 +153,6 @@ function validateCheckboxes() {
    
 }
 
-//header
-fetch('./header.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('navbar').innerHTML = data;
-});
 
 
 
